ca1d8d4f63fedee1959354916e0e22077f5b03e8
Carlito Pardales <carlpardales@gmail.com>
2024-05-06 16:27:14 +1200

    Initial text formatting toolbar

--- src/index.js
+++ src/index.js
@@ -41,6 +41,24 @@
     };
   };
 
+  const handleItalize = () => {
+    // Using saved cell and row indices, query table to get the cell.
+    // Programmatically set text font style to 'italic'
+  };
+
+  const handleBold = () => {
+    // Using saved cell and row indices, query table to get the cell.
+    // Programmatically set fontWeight = "bold"
+  };
+
+  const handleUndelrine = () => {
+    // Using saved cell and row indices, query table to get the cell.
+    // Programmatically set text decoration = 'underline'
+  };
+
+
+
+
   const drawSpreadsheet = () => {
     const container = document.getElementById("spreadsheetContainer");
     container.append(spreadsheet.new(handleCellUpdate));
